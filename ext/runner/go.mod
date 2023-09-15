88f55acfeb63e2d0de42763cae7d3d3e979af7b9
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-09-15 14:40:18 +0100

    Added simple test for ext
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

---
+++ ext/runner/go.mod
@@ -0,0 +1,30 @@
+module github.com/loopholelabs/scale-ext-example
+
+go 1.20
+
+replace github.com/loopholelabs/scale => /home/jimmy/code/scale/scale
+
+replace signature => /home/jimmy/.config/scale/signatures/local_testsig_latest_e6ddebc792ee929e2654b4281baca1376e05bf5a96d4bdf63a05a2aab5f9e749_signature/golang/host
+
+replace HttpFetch => /home/jimmy/.config/scale/extensions/local_testext_latest_5c7d22390f9101d459292d76c11b5e9f66c327b1766aae34b9cc75f9f40e8206_extension/golang/host
+
+require signature v0.1.0
+
+require github.com/loopholelabs/scale v0.3.19
+
+require (
+	HttpFetch v0.0.0-00010101000000-000000000000 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/hashicorp/hcl/v2 v2.18.0 // indirect
+	github.com/loopholelabs/polyglot v1.1.3 // indirect
+	github.com/loopholelabs/scale-signature-interfaces v0.1.7 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/tetratelabs/wazero v1.5.0 // indirect
+	github.com/zclconf/go-cty v1.13.2 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
---
+++ ext/runner/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+
+	"HttpFetch"
+	sig "signature"
+
+	scale "github.com/loopholelabs/scale"
+	scalefunc "github.com/loopholelabs/scale/scalefunc"
+)
+
+type FetchExtension struct {
+}
+
+type HttpConnector struct {
+}
+
+func (fe *FetchExtension) New(c *HttpFetch.HttpConfig) (HttpFetch.HttpConnector, error) {
+	return &HttpConnector{}, nil
+}
+
+func (hc *HttpConnector) Fetch(u *HttpFetch.ConnectionDetails) (HttpFetch.HttpResponse, error) {
+	r := HttpFetch.HttpResponse{}
+	// Do the actual fetch here...
+
+	resp, err := http.Get(u.Url)
+	if err != nil {
+		return r, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return r, err
+	}
+
+	return HttpFetch.HttpResponse{
+		StatusCode: int32(resp.StatusCode),
+		Body:       body,
+	}, nil
+}
+
+func main() {
+	fmt.Printf("Running scale function with ext...\n")
+
+	s, err := scalefunc.Read("../local-testfn-latest.scale")
+	if err != nil {
+		panic(err)
+	}
+
+	ext_impl := &FetchExtension{}
+	fns := HttpFetch.InstallExtension(ext_impl)
+
+	ctx := context.Background()
+
+	config := scale.NewConfig(sig.New).WithContext(ctx).WithFunctions([]*scalefunc.Schema{s}).WithExtensions(fns)
+
+	r, err := scale.New(config)
+	if err != nil {
+		panic(err)
+	}
+
+	i, err := r.Instance(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	sigctx := sig.New()
+
+	sigctx.Context.MyString = "hello world"
+	err = i.Run(context.Background(), sigctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Data from scaleFunction: %s\n", sigctx.Context.MyString)
+}
