a44d8cce24855a8ca2290d80aad7ece3650ee011
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-09-25 15:10:30 +0100

    Updated rust function code to test HttpFetch
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- ext/fn_code.rs
+++ ext/fn_code.rs
@@ -1,6 +1,32 @@
 
 use signature::types;
+use HttpFetch::types as fetch;
 
 pub fn scale(ctx: Option<&mut types::Context>) -> Result<Option<types::Context>, Box<dyn std::error::Error>> {
-    return signature::next(ctx);
+
+    let c = fetch::HttpConfig{ timeout: 10 };
+
+    let fetcher = fetch::New(c);
+
+    if fetcher.is_none() {
+      // Return an error...
+      let val = ctx.unwrap();
+      val.my_string = "Error".to_string();
+      return signature::next(Some(val));
+    }
+
+    let res = fetcher.unwrap().Fetch(fetch::ConnectionDetails{ url: "https://ifconfig.me".to_string() });
+
+    if res.is_none() {
+      // Return an error...
+      let val = ctx.unwrap();
+      val.my_string = "Error".to_string();
+      return signature::next(Some(val));
+    }
+
+    let val = ctx.unwrap();
+
+    val.my_string = format!("Fetch extension StatusCode={} Body={}", res.StatusCode, res.Body).to_string();
+
+    return signature::next(Some(val));
 }
