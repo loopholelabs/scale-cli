16483d61fce26fadf7e33cbeee0688d6af3e77a8
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-09-28 12:01:26 +0100

    Latest updates
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- ext/fn_code.rs
+++ ext/fn_code.rs
@@ -1,32 +1,57 @@
 
 use signature::types;
 use HttpFetch::types as fetch;
+use HttpFetch::New;
+use HttpFetch::HttpConnector;
 
 pub fn scale(ctx: Option<&mut types::Context>) -> Result<Option<types::Context>, Box<dyn std::error::Error>> {
 
     let c = fetch::HttpConfig{ timeout: 10 };
 
-    let fetcher = fetch::New(c);
+    let fetcher = New(c);
 
-    if fetcher.is_none() {
+    if fetcher.is_err() {
       // Return an error...
       let val = ctx.unwrap();
       val.my_string = "Error".to_string();
       return signature::next(Some(val));
     }
 
-    let res = fetcher.unwrap().Fetch(fetch::ConnectionDetails{ url: "https://ifconfig.me".to_string() });
+    let f = fetcher.unwrap();
 
-    if res.is_none() {
+    if f.is_none() {
       // Return an error...
       let val = ctx.unwrap();
       val.my_string = "Error".to_string();
       return signature::next(Some(val));
     }
+
+
+    let res = f.unwrap().Fetch(fetch::ConnectionDetails{ url: "https://ifconfig.me".to_string() });
+
+    if res.is_err() {
+      // Return an error...
+      let val = ctx.unwrap();
+      val.my_string = "Error".to_string();
+      return signature::next(Some(val));
+    }
+
+    let f1 = res.unwrap();
+
+    if f1.is_none() {
+      // Return an error...
+      let val = ctx.unwrap();
+      val.my_string = "Error".to_string();
+      return signature::next(Some(val));
+    }
+
+    let r = f1.unwrap();
 
     let val = ctx.unwrap();
 
-    val.my_string = format!("Fetch extension StatusCode={} Body={}", res.StatusCode, res.Body).to_string();
+    let string = String::from_utf8(r.body);
+
+    val.my_string = format!("Fetch extension StatusCode={} Body={}", r.status_code, string.unwrap()).to_string();
 
     return signature::next(Some(val));
 }
