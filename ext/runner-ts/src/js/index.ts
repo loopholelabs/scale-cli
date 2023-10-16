2c361651f03daa275326cdebc772dac6a796614a
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-10-16 13:49:52 +0100

    Added ts tests for exception thrown in extension.
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- ext/runner-ts/src/js/index.ts
+++ ext/runner-ts/src/js/index.ts
@@ -27,9 +27,15 @@ import { HttpFetchIfc, HttpConnector, New as FetchNew } from "@loopholelabs/Http
 
 let scale_runtime: Scale<Signature>;
 
+let error_new = false;
+let error_fetch = false;
+
 // An HttpConnector
 class HttpConnectorImpl implements HttpConnector {
   public Fetch(d: ConnectionDetails): HttpResponse {
+    if (error_fetch) {
+      throw new Error("Error from fetch");
+    }
     console.log("#EXT# HttpConnector.Fetch()", d);
     // TODO: Implement actual fetch logic here...
     const r = new HttpResponse();
@@ -44,80 +50,11 @@ class HttpConnectorImpl implements HttpConnector {
 // Make the extension implementation
 class Fetcher implements HttpFetchIfc {
   public New(c: HttpConfig): HttpConnector {
+    if (error_new) {
+      throw new Error("Error from new");
+    }
     console.log("#EXT# New()", c);
     return new HttpConnectorImpl();
-  }
-}
-
-// Make something that implements the bits from Extension
-class Ext implements Extension {
-  implementation: HttpFetchIfc;
-  instances_map: HttpConnectorImpl[];
-
-  constructor(i: HttpFetchIfc) {
-    this.implementation = i;
-    this.instances_map = [];
-  }
-
-  public Init(): Map<string, InstallableFunc> {
-    let fns = new Map<string, InstallableFunc>();
-
-// Add function for
-// ext_5c7d22390f9101d459292d76c11b5e9f66c327b1766aae34b9cc75f9f40e8206_HttpConnector_Fetch
-
-    const fetchfn = function(t) {
-      return (mem: ModuleMemory, resize: Resizer, params: number[]) => {
-        const id = params[0];
-        const ii = t.instances_map[id];
-        const d = mem.Read(params[1], params[2]);
-        const c = ConnectionDetails.decode(new Decoder(d));
-        if (c==undefined) {
-          throw new Error("ConnectionDetails is undefined");
-        }
-
-        const r = ii.Fetch(c);
-        const e = new Encoder();
-        r.encode(e);
-        const data = e.bytes;
-
-        const ptr = resize("ext_5c7d22390f9101d459292d76c11b5e9f66c327b1766aae34b9cc75f9f40e8206_Resize", data.length);
-  
-        mem.Write(ptr, data);
-
-        params[0] = 0;
-      }
-    }(this)
-
-    fns.set("ext_5c7d22390f9101d459292d76c11b5e9f66c327b1766aae34b9cc75f9f40e8206_HttpConnector_Fetch", fetchfn);
-
-    let newfn = function(t) {
-      return (mem: ModuleMemory, resize: Resizer, params: number[]) => {
-        const d = mem.Read(params[1], params[2]);
-        const c = HttpConfig.decode(new Decoder(d));
-
-        const con = t.implementation.New(c);
-        // Store it in instances map...
-
-        const id = t.instances_map.length;
-
-        t.instances_map.push(con);
-
-        // throw new Error("Testing error...");
-        // Return an instance
-        params[0] = id;
-      }
-    }(this);
-
-    fns.set("ext_5c7d22390f9101d459292d76c11b5e9f66c327b1766aae34b9cc75f9f40e8206_New", newfn);
-
-
-    console.log("Adding some functions into the mix...", fns);
-
-    return fns;
-  }
-
-  public Reset() {
-
   }
 }
 
@@ -142,32 +79,54 @@ async function init() {
     const sfn_rs = ScaleFunc.Decode(data_rs);
 
     let impl = new Fetcher();
-    let ex = new Ext(impl);
 
-    let ex2 = FetchNew(impl);
+    let ex = FetchNew(impl);
 
-    console.log("Old fetch", ex);
-    console.log("New fetch", ex2);
-
-    let config = new Config(SigNew);
-    config.WithFunctions([sfn_go]);
-    config.WithExtension(ex2);
-
-    scale_runtime = await New(config);
+  // First try go
+  error_new = false;
+  error_fetch = false;
+  console.log("\nRunning go tests...\n");
+  await runTest([sfn_go], ex);
+  error_new = true;
+  console.log("\nRunning go tests [ error from new ]...\n");
+  await runTest([sfn_go], ex);
+  error_new = false;
+  error_fetch = true;
+  console.log("\nRunning go tests [ error from fetch ]...\n");
+  await runTest([sfn_go], ex);
   
+  error_new = false;
+  error_fetch = false;
+  console.log("\nRunning rust tests...\n");
+  await runTest([sfn_rs], ex);
+  error_new = true;
+  console.log("\nRunning rust tests [ error from new ]...\n");
+  await runTest([sfn_rs], ex);
+  error_new = false;
+  error_fetch = true;
+  console.log("\nRunning rust tests [ error from fetch ]...\n");
+  await runTest([sfn_rs], ex);
+
   } catch(e) {
     console.log("Exception setting up scale functions", e);
-  }  
+  }
+}
 
-  const ctx = SigNew();
-  ctx.context.myString = "Hello world";
-
+async function runTest(functions: ScaleFunc[], ex: Extension) {
   try {
+    // First try go
+    let config = new Config(SigNew);
+    config.WithFunctions(functions);
+    config.WithExtension(ex);
+    scale_runtime = await New(config);
+
+    const ctx = SigNew();
+    ctx.context.myString = "Hello world";
+
     const i = await scale_runtime.Instance(undefined);
     await i.Run(ctx);
 
     console.log("Output was (" + ctx.context.myString + ")");
-
   } catch(e) {
       // There was an error! Need to write an error to context etc
       console.log("Error from scale", e);   
